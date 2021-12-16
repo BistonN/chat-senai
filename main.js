74b920a2cf34d6793ca788438b47bbf5d545c07e
bistonN <biston.nunes@gmail.com>
2021-12-16 14:20:15 -0300

    subindo arquivos chat

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// Create a client instance
+var client = new Paho.Client('broker.emqx.io', 8083, "clientId1");
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe("World");
+  message = new Paho.Message("clientId1 Hello");
+  message.destinationName = "World";
+  client.send(message);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
